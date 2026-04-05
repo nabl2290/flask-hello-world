c46d3fe3dc65ccb480ffa96ab2017042671f0484
Nate Bliton <nabl2290@colorado.edu>
2026-04-05 22:54:47 +0000

    add db_insert

--- app.py
+++ app.py
@@ -36,3 +36,22 @@ def creating():
     conn.close()
 
     return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://lab_10_db_dvqw_user:J931DPwmHL8EHhkXL810pNeiLDZcgQfC@dpg-d79e5s6a2pns73e3fjn0-a/lab_10_db_dvqw")
+
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    
+    conn.commit()
+    conn.close()
+
+    return "Basketball Table Successfully Populated"
