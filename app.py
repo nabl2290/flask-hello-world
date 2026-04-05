b7511e0e3ae8315ec08b4ede725b97c8afcc3181
Nate Bliton <nabl2290@colorado.edu>
2026-04-05 22:51:26 +0000

    add initial db_create route

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Nate Bliton nabl2290
+# Spring 2026 CSPB 3308
+
 import psycopg2
 
 from flask import Flask
@@ -13,3 +16,23 @@ def testing():
 
     conn.close()
     return "Databse Connection Successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://lab_10_db_dvqw_user:J931DPwmHL8EHhkXL810pNeiLDZcgQfC@dpg-d79e5s6a2pns73e3fjn0-a/lab_10_db_dvqw")
+
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    
+    conn.commit()
+    conn.close()
+
+    return "Basketball Table Successfully Created"
